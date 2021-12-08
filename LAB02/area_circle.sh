74611869831ac35d2d953c150e77abfd0aeabcd6
MazinSalim1 <71492673+MazinSalim1@users.noreply.github.com>
2021-12-08 22:19:15 +0530

    Add files via upload

---
+++ LAB02/area_circle.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+echo "Enter the radius of the circle: "
+read radius
+echo "Area of the circle: "
+echo "3.14 * $radius * $radius" | bc
+
---
+++ LAB12/sum_naturalnos.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo -n "Enter a number : "
+read num
+sum=0
+
+for (( i=1 ; i<=$num ; i++ ))
+do
+	sum=$(( $sum + $i ))
+done
+
+echo "Sum of $num natural numbers = $sum"
+
